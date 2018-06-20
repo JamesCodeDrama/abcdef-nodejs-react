0ac8fdcab20476edb46997f9f91d883912480106
Jirasin Chatviset <mymini_story@hotmail.com>
2018-06-20 10:40:14 +0700

    db_getter

--- server/coin_db_getter.js
+++ server/coin_db_getter.js
@@ -1,1 +1,20 @@
-// not started.
+var { request, async } = require('./express_connector')
+var request = require('request');
+var async = require('async');
+const app = express();
+const port = process.env.PORT || 5000;
+app.set('json spaces', 2);
+// var Quotes = require('./model/quotes_schema')
+var Coin = require('./model/coin_schema')
+var CoinMap = require('./model/coin_map_schema')
+
+app.get('/api/fromDB', (req, res) => {
+  Coin.find({}, [], 
+  {skip:0, limit:200, sort:{ rank: 1}}, 
+  function(err, coins) {
+    if (err) throw err;
+    else res.send({DB: coins});
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
